cbfe860f9607eef013a5562f424585771da16746
Mike Gale <mikeg@artefactual.com>
2013-05-06 16:01:23 -0700

    Move up date range box on page

--- apps/qubit/modules/search/templates/_advancedSearch.php
+++ apps/qubit/modules/search/templates/_advancedSearch.php
@@ -1,11 +1,32 @@
 <?php echo $form->renderFormTag(url_for(array('module' => 'search', 'action' => $action)), array('name' => 'form', 'method' => 'get')) ?>
 
 <?php if (isset($form->confirm)): ?>
-
   <?php echo $form->renderHiddenFields() ?>
   <div style="display: none;">
-
 <?php endif; ?>
+
+<div class="form-item">
+  <label><?php echo __('Date range search') ?></label>
+  <table>
+    <tr>
+      <td>
+        <?php echo $form->startDate
+          ->label(__('Start date'))
+          ->renderRow() ?>
+      </td>
+      <td>
+        <?php echo $form->endDate
+          ->label(__('End date'))
+          ->renderRow() ?>
+      </td>
+    </tr>
+    <tr>
+      <td colspan="2"><?php echo __('Date format required: YYYYMMDD') ?></td>
+    </tr>
+  </table>
+</div>
+
+
 
 <table class="multiRow" style="white-space: nowrap; min-width: 660px;">
   <tbody>
@@ -16,27 +37,6 @@
 <fieldset class="collapsible <?php echo ($form->getValue('repository') . $form->getValue('media') . $form->getValue('hasDigitalObject') . $form->getValue('levelOfDescription') . $form->getValue('startDate') . $form->getValue('endDate') ? '' : 'collapsed') ?>" id="filterLimit">
 
   <legend><?php echo __('Filter/Limit') ?></legend>
-
-  <div class="form-item">
-    <label><?php echo __('Date range search') ?></label>
-    <table>
-      <tr>
-        <td>
-          <?php echo $form->startDate
-            ->label(__('Start date'))
-            ->renderRow() ?>
-        </td>
-        <td>
-          <?php echo $form->endDate
-            ->label(__('End date'))
-            ->renderRow() ?>
-        </td>
-      </tr>
-      <tr>
-        <td colspan="2"><?php echo __('Date format required: YYYYMMDD') ?></td>
-      </tr>
-    </table>
-  </div>
 
   <?php if (sfConfig::get('app_multi_repository')): ?>
 
